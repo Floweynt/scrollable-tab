c40b27b6c7c650f7f728d51c706bea923de90826
Floweynt <guywithanaxe42@gmail.com>
2024-05-20 10:34:41 -0500

    chore: port to 1.20.3-1.20.6

--- src/main/java/com/floweytf/tabscroll/scrollabletab/compat/betterping/BetterPingCompatHandler.java
+++ src/main/java/com/floweytf/tabscroll/scrollabletab/compat/betterping/BetterPingCompatHandler.java
@@ -4,6 +4,7 @@ import com.floweytf.tabscroll.scrollabletab.compat.ICompatHandler;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.PlayerTabOverlay;
 import net.minecraft.client.multiplayer.PlayerInfo;
 
@@ -11,7 +12,7 @@ import java.lang.reflect.InvocationTargetException;
 
 public class BetterPingCompatHandler implements ICompatHandler {
     public interface BetterPingCallback {
-        void render(Minecraft client, PlayerTabOverlay hud, PoseStack context, int width, int x, int y, PlayerInfo player);
+        void render(Minecraft client, PlayerTabOverlay hud, GuiGraphics context, int width, int x, int y, PlayerInfo player);
     }
 
     public static BetterPingCallback callback;
@@ -24,7 +25,7 @@ public class BetterPingCompatHandler implements ICompatHandler {
                     Class.forName("com.vladmarica.betterpingdisplay.hud.CustomPlayerListHud")
                         .getMethod(
                             "renderPingDisplay",
-                            Minecraft.class, PlayerTabOverlay.class, PoseStack.class, int.class, int.class, int.class, PlayerInfo.class
+                            Minecraft.class, PlayerTabOverlay.class, GuiGraphics.class, int.class, int.class, int.class, PlayerInfo.class
                         );
                 callback = (mc, h, s, w, x, y, p) -> {
                     try {
